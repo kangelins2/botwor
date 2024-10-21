167bd17dcabb9ae4d194959920424a9ca2a7482b
kangelins2 <kirilova.angelina.kaktysik@gmail.com>
2024-10-21 19:26:48 +0300

    fix

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from anyio.abc import TaskGroup
 import dp
 import pandas as pd
 import uvicorn
-
+from sqlalchemy import select
 from pydantic import BaseModel
 from fastapi import FastAPI
-from aiogram import Dispatcher, Bot, F
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, declarative_base
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, Message, CallbackQuery, FSInputFile
+from aiogram import Dispatcher, Bot
+from sqlalchemy.orm import Mapped, mapped_column, declarative_base
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo, Message, FSInputFile
 
 from aiogram.filters import CommandStart, Command
 from dotenv import load_dotenv
@@ -39,19 +39,15 @@ class BaseORM(Base):
 
 
 class Email(BaseORM):
-    __tablename__ = 'emails'
+    __tablename__ = '__emails__'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str]
 
 
 def db_setup():
-    db_engine = create_engine(os.getenv("DATABASE_URL"))
-
-    db_session_factory = sessionmaker(bind=db_engine)
-    with db_session_factory() as session:
-        BaseORM.metadata.create_all(db_engine)
-        session.commit()
+    db_engine = create_async_engine(os.getenv("DATABASE_URL"))
+    db_session_factory = async_sessionmaker(db_engine)
     return db_engine, db_session_factory
 
 
@@ -65,10 +61,10 @@ class EmailRequest(BaseModel):
 # Определяем маршрут для обработки POST-запроса
 @app.post("/emails/")
 async def save_email(email: EmailRequest):
-    with session_factory() as db:
+    async with session_factory() as db:
         email_db = Email(email=email.email)
         db.add(email_db)
-        db.commit()
+        await db.commit()
     # Здесь вы можете добавить логику для обработки данных, например, сохранение в базе данных
     return {"status": "Ok"}
 
@@ -79,18 +75,16 @@ async def url_command(message: Message):
 
 
 ikb_donate = InlineKeyboardMarkup(row_width=1, inline_keyboard=[[
-                                          InlineKeyboardButton(text='let’s go',
-                                                                    web_app=WebAppInfo(url=f'https://google.com'))
-                                      ]
-                                  ])
+    InlineKeyboardButton(text='let’s go', web_app=WebAppInfo(url=f'https://google.com'))
+]])
 
 
 @dp.message(Command('download_emails'))
 async def download_price(message: Message):
-    with session_factory() as db:
-        email_list_db = db.query(Email).all()
+    async with session_factory() as db:
+        email_list_db = await db.execute(select(Email))
         email_list = []
-        for email in email_list_db:
+        for email in email_list_db.scalars().fetchall():
             email_list.append(email.email)
     df = pd.DataFrame(email_list, columns=['Emails'])
     csv_file = 'emails.csv'
@@ -98,6 +92,11 @@ async def download_price(message: Message):
     document = FSInputFile('emails.csv')
     await message.answer_document(document)
 
+async def get_email_list():
+    async with session_factory() as session:
+        result = await session.execute(select(Email))
+        email_list_db = result.scalars().all()
+        return [email.email for email in email_list_db]
 
 async def start_bot(tg: TaskGroup):
     bot = Bot(token=os.getenv("TG_BOT_TOKEN"))
@@ -114,6 +113,8 @@ async def main():
     config = uvicorn.Config(
         "main:app", "0.0.0.0", log_level=4, workers=1, reload=False,
     )
+    async with engine.begin() as session:
+        await session.run_sync(Base.metadata.create_all)
 
     server = uvicorn.Server(config)
     async with anyio.create_task_group() as tg:
